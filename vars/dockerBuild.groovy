282e0c1457239c9f44245c263be0b4d84ddb5dcb
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-16 23:58:19 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,10 @@
-def dockerFilePath = "docker/Dockerfile"
-def appName = env.JOB_NAME.split("/").last()
-def dockerRepoLink = env.DOCKER_REPO_LINK
+def dockerBuild(String dockerFilePath = 'docker/Dockerfile', String appName = env.JOB_NAME, String dockerRepoLink = '') {
+    // Define Docker image name and tag
+    def dockerImageName = "${appName}:${env.BUILD_NUMBER}"
+    // Build Docker image
+    sh "docker build -t ${dockerImageName} -f ${dockerFilePath} ."
+    // Tag Docker image with repository link
+    if (dockerRepoLink) {
+        sh "docker tag ${dockerImageName} ${dockerRepoLink}/${dockerImageName}"
+    }
+}

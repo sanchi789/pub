dd47324c2d6a6c4580551fb217f25b4a79bbabf6
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-19 10:30:12 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,11 @@
 def call(String dockerFilePath = 'docker/Dockerfile', String appName) {
     dockerRepoLink = env.DOCKER_REPO_LINK
+    try {
+    // some code that might throw an exception
+} catch (Exception e) {
+        error "Invalid input:  ${DOCKER_REPO_LINK} "
+}
+
     // Define Docker image name and tag
     def dockerImageName = "${appName}:${env.BUILD_NUMBER}"
     // Build Docker image

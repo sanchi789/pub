894d400e0cad5a7748741b53f0c3b8f3d0ef3736
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-24 17:54:13 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -24,6 +24,8 @@ sh '''
 
 git config --global user.name "sanchi789"
 git config --global user.email "sanchi.sharma1@tothenew.com"
+git add .
+git status
 git push --set-upstream origin main
 git commit -m "Publish new version"
 git push

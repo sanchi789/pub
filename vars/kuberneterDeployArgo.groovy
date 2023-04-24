6818148f6722f2c70af255c9eb6108adba7d696e
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-24 18:06:30 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -1,24 +1,7 @@
 def call(String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret) {
-//     def appName = "my-app"             // Set the application name
-//     def namespace = "argocd"          // Set the namespace to deploy the application
-//     def repoName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('.'))
-//    def chartName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('-'))
-//     appName = env.app_name
-//     namespace = env.ns
-//     repoName = env.repo
-//     chartName = env.chart
-
-//     stage("Checkout") {
-//         git url: "git@github.com:<your-repo>/<your-app>.git", branch: "main", credentialsId: gitSecret
-//     }
-
-//     stage("Update values.yaml") {
-//         sh "sed -i -E 's/tag: [^ ]*/tag: \"${newImageTag}\"/' ${valuesFilePath}"
-//        // sed -i -E 's/tag: [^ ]*/tag: "${DOCKER_IMAGE_TAG}"/' 'apps/charts/${App_Name}/values.yaml'
-//     }
 
 sh "sed -i -E 's/tag: [^ ]*/tag: \"${newImageTag}\"/' ${valuesFilePath}"
-//cat apps/charts/${App_Name}/values.yaml
+
   
 sh '''
 

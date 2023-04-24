bb3800bce14bb46ad6264a6715921f1904e63345
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-24 14:22:28 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -1,8 +1,8 @@
-def call(String helmRepoLink, String valuesFilePath, String newImageTag, String gitSecret) {
-    def appName = "my-app"             // Set the application name
-    def namespace = "default"          // Set the namespace to deploy the application
-    def repoName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('.'))
-    def chartName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('-'))
+def call(String helmRepoLink, String valuesFilePath, String newImageTag) {
+//     def appName = "my-app"             // Set the application name
+//     def namespace = "default"          // Set the namespace to deploy the application
+//     def repoName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('.'))
+//     def chartName = helmRepoLink.substring(helmRepoLink.lastIndexOf('/') + 1, helmRepoLink.lastIndexOf('-'))
 
 //     stage("Checkout") {
 //         git url: "git@github.com:<your-repo>/<your-app>.git", branch: "main", credentialsId: gitSecret
@@ -12,19 +12,15 @@ def call(String helmRepoLink, String valuesFilePath, String newImageTag, String 
         sh "sed -i -E 's/tag: [^ ]*/tag: \"${newImageTag}\"/' ${valuesFilePath}"
     }
 
-    stage("Helm repo add") {
-        sh "helm repo add ${repoName} ${helmRepoLink}"
-        sh "helm repo update"
-    }
 
-    stage("Helm upgrade") {
-        sh "helm upgrade --install ${appName} ${repoName}/${chartName} --namespace ${namespace} -f ${valuesFilePath}"
-    }
+//     stage("Helm upgrade") {
+//         sh "helm upgrade --install ${appName} ${repoName}/${chartName} --namespace ${namespace} -f ${valuesFilePath}"
+//     }
 
-    stage("Argo CD sync") {
-        def syncResult = sh(script: "argocd app sync ${appName} --namespace ${namespace}", returnStatus: true)
-        if (syncResult != 0) {
-            error "Failed to sync Argo CD application"
-        }
-    }
+//     stage("Argo CD sync") {
+//         def syncResult = sh(script: "argocd app sync ${appName} --namespace ${namespace}", returnStatus: true)
+//         if (syncResult != 0) {
+//             error "Failed to sync Argo CD application"
+//         }
+//     }
 }

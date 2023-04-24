424cf6cbb78bcab7028c88270203388a252b0d9e
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-24 15:30:30 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -19,7 +19,10 @@ def call(String helmRepoLink, String valuesFilePath, String newImageTag) {
 
 
     stage("Helm upgrade") {
-        sh "helm upgrade --install ${appName} ${chartName} --namespace ${namespace} -f ${valuesFilePath}"
+        //sh "helm upgrade --install ${appName} ${repoName}/${chartName} --namespace ${namespace} -f ${valuesFilePath}"
+        sh "helm install ${chartName} ${appName}"
+        sh "helm upgrade ${appName}"
+
     }
 
 //     stage("Argo CD sync") {

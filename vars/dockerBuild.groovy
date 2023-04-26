82b46491465c9fb2ee147c3da920544b3772e952
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-04-26 15:30:48 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -7,7 +7,7 @@ def call(String dockerFilePath = 'docker/Dockerfile', String appName,String dock
 }
 
     // Define Docker image name and tag
-    def dockerImageName = "${appName}:${env.BUILD_NUMBER}"
+    def dockerImageName = "${appName}:${env.DOCKER_IMAGE_TAG = sh(script: "printf \$(git rev-parse --short HEAD)", returnStdout: true )}"
     // Build Docker image
     sh "docker build -t ${dockerRepoLink}/${dockerImageName} ."
 }

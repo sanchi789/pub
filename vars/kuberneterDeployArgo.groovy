028125679e2e0c9b665081e1b0eea6acba779e2a
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-01 15:59:53 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -4,8 +4,6 @@ sh "sed -i -E 's/tag: [^ ]*/tag: \"${newImageTag}\"/' ${valuesFilePath}"
 
   
 sh '''
-git config --global user.name "sanchi789"
-git config --global user.email "sanchi.sharma1@tothenew.com"
 git add .
 git status
 git push --set-upstream origin main

0765867d6d62d1316aaeb07ca3f1a6ca4b56672c
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-01 15:16:58 +0530

    Update kuberneterDeployArgo.groovy

--- vars/kuberneterDeployArgo.groovy
+++ vars/kuberneterDeployArgo.groovy
@@ -1,12 +1,11 @@
-def call(String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret) {
+def call(String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret String username,String email) {
 
 sh "sed -i -E 's/tag: [^ ]*/tag: \"${newImageTag}\"/' ${valuesFilePath}"
 
   
 sh '''
-
-git config --global user.name "sanchi789"
-git config --global user.email "sanchi.sharma1@tothenew.com"
+git config --global user.name "${username}"
+git config --global user.email "${email}"
 git add .
 git status
 git push --set-upstream origin main

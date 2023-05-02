62809f505f8da81ed4ca84f038d8737ccf4f0bec
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-02 15:56:00 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -2,6 +2,6 @@ def call(String jenkinsSecret, String repoLink, String branchName)
 {
    git branch: "${branchName}", credentialsId: "${jenkinsSecret}", url: "${repoLink}"  
    
-    env.DOCKER_IMAGE_TAG = sh(script: "printf \$(git rev-parse --short HEAD)", returnStdout: true )
+//     env.DOCKER_IMAGE_TAG = sh(script: "printf \$(git rev-parse --short HEAD)", returnStdout: true )
         
-}
+// }

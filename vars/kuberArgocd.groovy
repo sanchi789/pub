c80eba5e2e81820c54ec439712e83030071aff84
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-02 12:12:32 +0530

    Update kuberArgocd.groovy

--- vars/kuberArgocd.groovy
+++ vars/kuberArgocd.groovy
@@ -1,4 +1,5 @@
 // import com.tothenew.Utilities
+k
 def call(String jenkinsSecret, String repoLink, String branchName,String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret){
  
 pipeline {

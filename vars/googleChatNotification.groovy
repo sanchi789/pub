58bc15ee595531ced4e5d7d38fd7434fa4ebddcb
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-10 09:48:15 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -1,13 +1,3 @@
-// def call(String webhookUrl, String message) {
-//   def json = [:]
-//   json['text'] = message
-//   def body = JsonOutput.toJson(json)
-//   def response = httpRequest(url: webhookUrl, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: body)
-//   if (response.status != 200) {
-//     error("Failed to send Google Chat notification: ${response.status} ${response.content}")
-//   }
-// 
-
 import groovy.json.JsonOutput
 import org.apache.http.HttpEntity
 import org.apache.http.client.methods.CloseableHttpResponse

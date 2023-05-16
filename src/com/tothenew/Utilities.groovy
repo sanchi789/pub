8c765711712736b41f0ca481dc27f789f02c5f35
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-16 14:53:30 +0530

    Update Utilities.groovy

--- src/com/tothenew/Utilities.groovy
+++ src/com/tothenew/Utilities.groovy
@@ -21,10 +21,10 @@ def cleanupDockerImage() {
 }
 
 //kubernete deploy through argocd
-def kuberneterDeployArgo(String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret,String filePath,String branchName) {
+def kuberneterDeployArgo(String helmRepoLink, String valuesFilePath, String newImageTag,String gitSecret,String branchName) {
     //gitClone for helm 
     git branch: "${branchName}", credentialsId: "${gitSecret}", url: "${helmRepoLink}"
-    oldTag = sh(script: "grep -i \"image:\" ${filePath} | cut -d : -f 3" , returnStdout: true).trim()
+    oldTag = sh(script: "grep -i \"image:\" ${valuesFilePath} | cut -d : -f 3" , returnStdout: true).trim()
     sh "sed -i \'s/${oldTag}/${newImageTag}/g\' ${valuesFilePath}"
     sh "echo ${valuesFilePath}"
     sh '''

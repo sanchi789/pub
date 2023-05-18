74a170bd5ba1e728bd303602cbf2c782ceb5d2f9
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-18 16:29:47 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -2,7 +2,7 @@ def getBuildUser() {
     return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
   
-def call(String channels,String tokens,String messages,String isSuccess)
+def call(String channels,String tokens,String isSuccess)
 {
   
   def COLOR_MAP = [
@@ -11,18 +11,18 @@ def call(String channels,String tokens,String messages,String isSuccess)
     'ABORT': 'warning'
 ]
   
-   if("${isSuccess}"=="true"){
+//    if("${isSuccess}"=="true"){
 
       BUILD_USER = getBuildUser()
          
  slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
-      }
+//       }
   
-      else{
-         BUILD_USER = getBuildUser()
+//       else{
+//          BUILD_USER = getBuildUser()
 
-           slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
+//            slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
 
     
-   }
+//    }
 }

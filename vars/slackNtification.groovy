3de4d571b2a182cc4f93003a805f059f10fbb34f
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-18 15:06:45 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -1,36 +1,12 @@
-// def call(String channel,String token,String message,boolean isSuccess)
-// {
-//    post {
-//       if(isSuccess==true){
-//         success {
-//             script {
-//                 //slackNotification('Build completed', true)
-//                slackSend( channel: "${channel}", token: "${token}", message: "${message}")
-//             }
-//         }
-//       }
-//       else{
-//         failure {
-//             script {
-//                 //slackNotification('Build failed', false)
-//                slackSend( channel: "${channel}", token: "${token}", message: "${message}")
-//             }
-//         }
-//     }
-//    }
-// }
 def call(String channels,String tokens,String messages,String isSuccess)
 {
    
    if("${isSuccess}"==true){
         
-                //slackNotification('Build completed', true)
                slackSend( channel: "${channels}", token: "${tokens}", message: "${messages}")
 
       }
       else{
-        
-                //slackNotification('Build failed', false)
                slackSend( channel: "${channels}", token: "${tokens}", message: "${messages}")
     
    }

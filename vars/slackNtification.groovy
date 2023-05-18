7b484d6e47e2d8aec0d2651685532e343cc2bd95
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-18 16:00:17 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -1,16 +1,29 @@
 def call(String channels,String tokens,String messages,String isSuccess)
 {
   
+  def COLOR_MAP = [
+    'SUCCESS': 'good', 
+    'FAILURE': 'danger',
+]
+  
+  
+  def getBuildUser() {
+    return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+}
+  
+  
    if("${isSuccess}"=="true"){
-      def successMessage = "*Job Succeeded* :white_check_mark:\n\n"
-      def formattedMessag = successMessage + "> ${messages}"
-               slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessag}")
-
+//       def successMessage = "*Job Succeeded* :white_check_mark:\n\n"
+//       def formattedMessag = successMessage + "> ${messages}"
+      BUILD_USER = getBuildUser()
+           //    slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessag}")
+ slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
       }
   
       else{
-         def failureMessage = ":x: *Job Failed* :x:\n\n"
-         def formattedMessage = failureMessage + "> ${messages}"
+         BUILD_USER = getBuildUser()
+//          def failureMessage = ":x: *Job Failed* :x:\n\n"
+//          def formattedMessage = failureMessage + "> ${messages}"
                slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessage}")
     
    }

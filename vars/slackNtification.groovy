1e920c2d00237d8951e5b57cdae7249908fd54a1
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-18 16:05:02 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -22,7 +22,9 @@ def call(String channels,String tokens,String messages,String isSuccess)
          BUILD_USER = getBuildUser()
 //          def failureMessage = ":x: *Job Failed* :x:\n\n"
 //          def formattedMessage = failureMessage + "> ${messages}"
-               slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessage}")
+             //  slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessage}")
+           slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
+
     
    }
 }

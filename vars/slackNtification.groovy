fb0f8a01957eb0abf2e1cdf9ab72a4a059738031
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-18 15:35:24 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -1,12 +1,13 @@
 def call(String channels,String tokens,String messages,String isSuccess)
 {
   
-   if("${isSuccess}"==true){
+   if("${isSuccess}"=="true"){
       def successMessage = "*Job Succeeded* :white_check_mark:\n\n"
-    def formattedMessag = successMessage + "> ${messages}"
+      def formattedMessag = successMessage + "> ${messages}"
                slackSend( channel: "${channels}", token: "${tokens}", message: "${formattedMessag}")
 
       }
+  
       else{
          def failureMessage = ":x: *Job Failed* :x:\n\n"
          def formattedMessage = failureMessage + "> ${messages}"

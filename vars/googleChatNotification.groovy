e0633cafc9aee7b644b527f8774956376036b4ea
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-19 12:50:09 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -36,11 +36,15 @@ def getBuildUser() {
     return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
 
-def call(String webhookUrl,String message) {
+def call(String webhookUrl) {
    BUILD_USER = getBuildUser()
- // def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
+ def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
   //sh 'curl -X POST -H 'Content-Type: application/json' -d '{\"text\": \"${message}"\}' "${webhookUrl}"'
-  sh "curl -X POST -H 'Content-Type: application/json' -d '{\"text\": \"${message}\"}' ${webhookUrl}"
+//   sh "curl -X POST -H 'Content-Type: application/json' -d '{\"text\": \"${message}\"}' ${webhookUrl}"
+  String curlCommand = "curl -X POST -H 'Content-Type: application/json' -d '{\"text\": \"${message}\"}' ${webhookUrl}"
+  sh curlCommand
+
+
 }
 
 

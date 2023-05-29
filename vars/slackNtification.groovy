ac6ac697ff17b45f980dc3a17aac5e8b3faf8ab3
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 12:19:07 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -5,11 +5,11 @@ def getBuildUser() {
 def call(String buildUrl)
 {
   
-  def COLOR_MAP = [
-    'SUCCESS': 'good', 
-    'FAILURE': 'danger',
-    'ABORT': 'warning'
-]
+//   def COLOR_MAP = [
+//     'SUCCESS': 'good', 
+//     'FAILURE': 'danger',
+//     'ABORT': 'warning'
+// ]
  BUILD_USER = getBuildUser()
          
  //slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"

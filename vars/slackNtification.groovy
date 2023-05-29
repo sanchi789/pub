8b78c31da0db5c2ca9e9e49da530c971b1ca0bb2
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 15:54:34 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -2,43 +2,26 @@ def getBuildUser() {
     return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
   
-def call(String buildUrl,String Status)
+def call(String buildUrl,String SzDezfdc tatus)
 {
   def color
   def COLOR_MAP = [
-//         switch (Status) {
-//         case "success":
-//             "SUCCESS" = "#36a64f"'
-//             break
-   Switch (status) {  
-    Case "Success":
-        color = "#36a64f"
-        statusText = "SUCCESS"
-        break
-    Case "Failure":
-        color = "#ff0000"
-        statusText = "FAILURE"
-        break
-    Case "Aborted":
-        color = "#f9c300"
-        statusText = "ABORTED"
-        break
-    default:
-        color = "#000000"
-        statusText = "UNKNOWN STATUS"
-        break
-}
+        switch (status) {
+        case "success":
+            "SUCCESS" = "#36a64f"'
+            break
+        case "failure":
+            //color = "#ff0000"
+             "FAILURE" =  "#ff0000"
+            break
+        case "aborted":
+           // color = "#f9c300"
+            "ABORTED"="#f9c300"
+            break
+        default:
+            break
+        }
       ]
-//         case "failure":
-//             //color = "#ff0000"
-//              "FAILURE" =  "#ff0000"
-//             break
-//         case "aborted":
-//            // color = "#f9c300"
-//             "ABORTED"="#f9c300"
-//             break
-//         default:
-//             break
  BUILD_USER = getBuildUser()
          
  //slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"

fe68c4c0f687ebaeee0905168f205a2a31c81e7e
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 17:47:30 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -6,14 +6,14 @@ def call(String buildUrl)
 {
   def color
   def COLOR_MAP = [
-      "SUCCESS": "good",
-      "FAILURE": "danger",
-      "ABORTED": "warning"
+      "SUCCESS": 'good',
+      "FAILURE": 'danger',
+      "ABORTED": 'warning'
       ]
  BUILD_USER = getBuildUser()
          
  //slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
-    def message= "*${COLOR_MAP[currentBuild.currentResult]}:* \n *Job_Name:* ${env.JOB_NAME}\n color: *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
+    def message= "*${COLOR_MAP[currentBuild.currentResult]} *${currentBuild.currentResult}:*\n *Job_Name:* ${env.JOB_NAME}\n color: *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
 
   sh  """
     curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"${message}\"}' ${buildUrl}

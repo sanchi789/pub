dcb1c17863bb610477020ebc758bfc0ca0838c09
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 12:51:16 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -5,15 +5,15 @@ def getBuildUser() {
 def call(String buildUrl)
 {
   
-  def COLOR_MAP = [
-    'success': 'good', 
-    'failure': 'danger',
-    'aborted': 'warning'
-]
+//   def COLOR_MAP = [
+//     'success': 'good', 
+//     'failure': 'danger',
+//     'aborted': 'warning'
+// ]
  BUILD_USER = getBuildUser()
          
  //slackSend channel: "${channels}", color: COLOR_MAP[currentBuild.currentResult], token: "${tokens}", message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
-  def message= "*${currentBuild.currentResult}:* "color: COLOR_MAP[currentBuild.currentResult]" Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
+  def message= "*${currentBuild.currentResult}:* Job_Name: ${env.JOB_NAME}\n build_Number ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
   //  curl -X POST -H 'Content-type: application/json' --data '{"text":"${messge}"}' https://hooks.slack.com/services/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
    // sh ' curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' {https://hooks.slack.com/services/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}'
   sh  """

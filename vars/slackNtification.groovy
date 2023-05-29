c17851b7632e6378b3159003b584d5cedc1b1d47
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 12:24:16 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -16,8 +16,5 @@ def call(String buildUrl)
   def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
   //  curl -X POST -H 'Content-type: application/json' --data '{"text":"${messge}"}' https://hooks.slack.com/services/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
    // sh ' curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' {https://hooks.slack.com/services/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}'
-  // sh" curl -X POST -H 'Content-type: application/json' --data '{"text":"HelloWorld"}' https://hooks.slack.com/services/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"
- // sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"${message}"}' https://hooks.slack.com/services/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"
-  //sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"HelloWorld"}' https://hooks.slack.com/services/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"
   sh  "curl -X POST -H 'Content-type: application/json' --data '{"text":"{message}"}' ${buildUrl}"
 }

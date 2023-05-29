8e2b218925b8db58f8d91af7bc1e3c26c9706e1f
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-29 11:07:05 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -2,7 +2,7 @@ def getBuildUser() {
     return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
   
-def call()
+def call(String buildUrl)
 {
   
   def COLOR_MAP = [
@@ -18,5 +18,6 @@ def call()
    // sh ' curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' {https://hooks.slack.com/services/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}'
   // sh" curl -X POST -H 'Content-type: application/json' --data '{"text":"HelloWorld"}' https://hooks.slack.com/services/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"
  // sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"${message}"}' https://hooks.slack.com/services/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"
-  sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"HelloWorld"}' https://hooks.slack.com/services/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"
+  //sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"HelloWorld"}' https://hooks.slack.com/services/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"
+  sh "curl -X POST -H 'Content-type: application/json' --data '{"text":"{message}"}' ${buildUrl}"  
 }

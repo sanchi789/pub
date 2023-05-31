9aa233bafb7ffd816c72ae7a371bff8572874edd
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-31 11:37:39 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -13,7 +13,7 @@ def call(String webhookUrl) {
      def message= "*${currentBuild.currentResult}:*\n *Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
    
  sh  """
-    curl -XPOST -H 'Content-type: application/json\; charset=UTF-8' -d '{ \"attachments\": [{ \"color\": \"${COLOR_MAP[currentBuild.currentResult]}\", \"text\": \"${message}\" }] }' ${webhookUrl}
+    curl -XPOST -H 'Content-type: application/json\; charset=UTF-8' -d "{"'"attachments"'": [{"'"color"'": "${COLOR_MAP[currentBuild.currentResult]}", "'"text"'": "${message}" }] }' ${webhookUrl}"
     """
     
     //curl -XPOST -H 'Content-Type: application/json\; charset=UTF-8' -d "{"'"text"'": "'"Hello from a Python script!"'"}" "https://chat.googleapis.com/v1/spaces/AAAAycUdkuk/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=KNgJ2QhUHmKD0MagNh09wfaSd3slF8-3iuBSNQVHsz0"

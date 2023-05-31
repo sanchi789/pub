43aa0fe493f90af53aa01fe060ec11043d8b14da
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-31 11:11:06 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -3,6 +3,11 @@ def getBuildUser() {
 }
 
 def call(String webhookUrl) {
+      def COLOR_MAP = [
+      "SUCCESS": 'good',
+      "FAILURE": 'danger',
+      "ABORTED": 'warning'
+      ]
    BUILD_USER = getBuildUser()
  //def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
      def message= "*${currentBuild.currentResult}:*\n *Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"

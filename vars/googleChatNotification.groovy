e01336ff339f8dca6ef8e66946bb5e0bc90e3d22
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-31 12:26:16 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -10,7 +10,7 @@ def call(String webhookUrl) {
       ]
    BUILD_USER = getBuildUser()
  //def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
-     def message= "*${currentBuild.currentResult}:*\n *Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
+    def message= "*${currentBuild.currentResult}:*\n *AppName:* ${env.app_name}\n*Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
    
 //  sh  """
 //     curl -XPOST -H 'Content-type: application/json\; charset=UTF-8' -d "{"'"attachments"'": [{"'"color"'": "${COLOR_MAP[currentBuild.currentResult]}", "'"text"'": "${message}" }] }' ${webhookUrl}"

db096f70714fc4f2f2f87161cae1f866c15063be
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-05-31 12:46:24 +0530

    Update googleChatNotification.groovy

--- vars/googleChatNotification.groovy
+++ vars/googleChatNotification.groovy
@@ -3,24 +3,13 @@ def getBuildUser() {
 }
 
 def call(String webhookUrl) {
-      def COLOR_MAP = [
-      "SUCCESS": 'good',
-      "FAILURE": 'danger',
-      "ABORTED": 'warning'
-      ]
-   BUILD_USER = getBuildUser()
- //def message= "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
-    def message= "*${currentBuild.currentResult}:*\n *AppName:* ${env.app_name}\n*Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
-   
-//  sh  """
-//     curl -XPOST -H 'Content-type: application/json\; charset=UTF-8' -d "{"'"attachments"'": [{"'"color"'": "${COLOR_MAP[currentBuild.currentResult]}", "'"text"'": "${message}" }] }' ${webhookUrl}"
-//     """
     
+   BUILD_USER = getBuildUser()
+    def message= "*${currentBuild.currentResult}:*\n *AppName:* ${env.app_name}\n*Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
+
     sh """ curl -XPOST -H 'Content-Type: application/json; charset=UTF-8' -d "{"'"text"'": "'"${message}"'"}" "${webhookUrl}"
     """
-//     sh """
-//       curl -XPOST -H 'Content-Type: application/json; charset=UTF-8' -d "{"'"text"'": "'"${message}"'"}" ${webhookUrl}
-//      """
+
 }
 
 

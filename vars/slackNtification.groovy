4c783ea2e095d050fdea9970d5cd5b41475c161e
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-06-01 10:29:49 +0530

    Update slackNtification.groovy

--- vars/slackNtification.groovy
+++ vars/slackNtification.groovy
@@ -11,7 +11,7 @@ def call(String buildUrl)
       ]
  BUILD_USER = getBuildUser()
          
-    def message= "*    ${currentBuild.currentResult}   *\n *AppName:* ${env.app_name}\n *Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
+    def message= "     *${currentBuild.currentResult}*    \n *AppName:* ${env.app_name}\n *Job_Name:* ${env.JOB_NAME}\n *Build_Number:* ${env.BUILD_NUMBER} \n *Build By* ${BUILD_USER}\n *More info at:* ${env.BUILD_URL}"
     sh  """
     curl -X POST -H 'Content-type: application/json'  --data '{ \"attachments\": [{ \"color\": \"${COLOR_MAP[currentBuild.currentResult]}\", \"text\": \"${message}\" }] }' ${buildUrl}
     """
